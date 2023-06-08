c20ff609e83c40a17dfc73aad2a9d7bd0c20a127
Usman <usman.zahid@xgrid.co>
2023-06-08 14:10:42 +0500

    First Customization

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,3 +4,13 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+
+resource "control_tower_account_factory_customization" "account_customization" {
+  type   = "account"
+  source = "aws-ia/control_tower_s3_logging/aws"
+  version = "0.1.0"
+
+  parameters = {
+    account_id = "818987027252"
+  }
+}
